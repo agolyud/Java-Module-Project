d0333c24285367dd3a576ab19604d556d0a86d5a
Aleksey Golyudov <98583104+agolyd@users.noreply.github.com>
2022-10-03 19:44:25 +0300

    YandexPraktikum v2.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,35 +4,55 @@ import java.util.List;
 import java.util.Scanner;
 public class Main {
 
+    static int persons = 0;
+    static int nextPrice = 0;
     public static Scanner in = new Scanner(System.in);
     public static PrintStream out = System.out;
 
     public static void main(String[] args) {
-        System.out.println("Привет Мир");
-        out.println("На скольких человек необходимо разделить счёт.");
-        int persons = in.nextInt();
-        while (persons <= 1) {
-            out.println("Количество человек меньше 1. Это некорректное значение для подсчёта.");
-            persons = in.nextInt();
-        }
+
+        System.out.println("На скольких человек необходимо разделить счёт.");
+            while (true) {
+                if (in.hasNext() && in.hasNextInt())
+                {
+                    persons = in.nextInt();
+                    if (persons <= 1){
+                        System.out.println("Некорректный ввод.\n" +
+                                "Введите количество человек больше 1.");
+                    }
+                    else {
+                        break;
+                    }
+                }
+                else{
+                    System.out.println("Некорректиный ввод.\n" +
+                            "Введите количество человек целым положительным числом.");
+                    in.next();
+                }
+            }
+
 
         List<Calculator> items = new ArrayList<>();
          out.println("Введите название товара");
-        String nextPosition = in.next();
+         String nextPosition = in.next();
+
         out.println("Введите стоимость в формате: 'рубли.копейки' [10,45, 11,40]");
         double nextPrice = in.nextDouble();
         int i = 0;
-        while (!nextPosition.equalsIgnoreCase("Завершить")) {
-            out.println("Хотите еще добавить товар?, если да то Введите название товара" +
-                    "\nДля завершения введите 'Завершить'");
-            items.add(new Calculator(nextPosition, nextPrice));
-            nextPosition = in.next();
-            if (!nextPosition.equalsIgnoreCase("Завершить")) {
-                out.println("Введите стоимость в формате: 'рубли.копейки' [10,45, 11,40]");
-                nextPrice = in.nextDouble();
+
+
+            while (!nextPosition.equalsIgnoreCase("Завершить")) {
+                out.println("Хотите еще добавить товар?, если да то Введите название товара" +
+                        "\nДля завершения введите 'Завершить'");
+                items.add(new Calculator(nextPosition, nextPrice));
+                nextPosition = in.next();
+                if (!nextPosition.equalsIgnoreCase("Завершить")) {
+                    out.println("Введите стоимость в формате: 'рубли.копейки' [10,45, 11,40]");
+                    nextPrice = in.nextDouble();
+                }
+                i += 1;
             }
-            i += 1;
-        }
+
         double sum = 0;
         for (int j = 0; j < i; j++)
         sum += items.get(j).price;
@@ -43,10 +63,12 @@ public class Main {
         switch ((int) ruble % 10) {
             case 1:
                 endingRuble = "рубль";
+                break;
             case 2:
             case 3:
             case 4:
                 endingRuble = "рубля";
+                break;
             default:
                 endingRuble = "рублей";
         }
